1d0c73cfb32f12ac6519cdb2074501c6b275e2e4
Peter Drotos <peter.drotos@outlook.com>
2026-06-26 12:58:07 +0200

    traffic_hash rename

--- gateware/src/src_hdl/fabric_bridge.sv
+++ gateware/src/src_hdl/fabric_bridge.sv
@@ -119,7 +119,7 @@ module fabric_bridge (
   wire         req_ovr_valid;
   wire [255:0] req_ovr_digest;
 
-  traffic_hash #(
+  traffic_commit #(
     .HDR_BYTES (16)
   ) hash_req (
     .clk      (clk),
@@ -197,7 +197,7 @@ module fabric_bridge (
   wire         rsp_ovr_valid;
   wire [255:0] rsp_ovr_digest;
 
-  traffic_hash #(
+  traffic_commit #(
     .HDR_BYTES (16)
   ) hash_rsp (
     .clk      (clk),
